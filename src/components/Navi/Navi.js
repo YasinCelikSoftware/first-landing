44bfc034bc0499f0b8205a201059bd6c9d613c9f
YasinCelikSoftware <yasin.celik4@outlook.com>
2023-07-27 21:45:49 +0300

    Sticky Nav and some css

--- src/components/Navi/Navi.js
+++ src/components/Navi/Navi.js
@@ -6,43 +6,65 @@ import {
   Nav,
   NavItem,
   NavLink,
+  Container,
+  Row,
 } from 'reactstrap';
 import NavBrand from './NavBrand';
 import './Navi.css';
+import {useEffect} from 'react';
 
 function Navi (args) {
   const [isOpen, setIsOpen] = useState (false);
+  const [stickyClass, setStickyClass] = useState ('relative');
 
   const toggle = () => setIsOpen (!isOpen);
 
+  useEffect (() => {
+    window.addEventListener ('scroll', stickyNavbar);
+    return () => {
+      window.removeEventListener ('scroll', stickyNavbar);
+    };
+  }, []);
+
+  const stickyNavbar = () => {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY;
+      console.log (windowHeight);
+      windowHeight > 50
+        ? setStickyClass ('bg-white')
+        : setStickyClass ('bg-transparent');
+    }
+  };
+
   return (
-    <div>
-      <Navbar {...args} expand="md" className="px-5">
-        <NavBrand />
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ms-auto" navbar>
-            <NavItem>
-              <NavLink href="#home">Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#about-us">About Us</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#our-services">Services</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#our-portfolio">Portfolio</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#testimonial">Contact Us</NavLink>
-            </NavItem>
-
-          </Nav>
-
-        </Collapse>
-      </Navbar>
-    </div>
+    <Navbar
+      {...args}
+      expand="md"
+      className={`px-10 ${stickyClass}`}
+      fixed="top"
+    >
+      <NavBrand />
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ms-auto" navbar>
+          <NavItem>
+            <NavLink href="#home">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#about-us">About Us</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#our-services">Services</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#our-portfolio">Portfolio</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#testimonial">Contact Us</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
   );
 }
 
